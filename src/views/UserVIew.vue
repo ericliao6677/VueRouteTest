886f7b6bda13f2f3f7d94847b128115a619ed670
ericliao <chixiang777@gmail.com>
2024-05-28 09:53:27 +0800

    feat: 新增動態路由匹配

--- src/views/UserVIew.vue
+++ src/views/UserVIew.vue
@@ -1,12 +1,16 @@
-<script>
-import { useRoute } from 'vue-router'
+<script setup>
+import { computed } from 'vue'; //TODO 動態路由匹配
+import { useRoute } from 'vue-router';
+const route = useRoute();
+const userId = computed(() => route.params.username);
 </script>
 <template>
   <div>
-    {{ }}
+    <h3 class="text-primary">this is user page</h3>
+    <h5>
+      User Id: <strong class="text-danger">{{ userId }}</strong>
+    </h5>
   </div>
 </template>
-
-
 
 <style></style>
